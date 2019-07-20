4d4a02595a709a4720e67fff7674a060de06a0e3
kotavishwanath <kotavishwanath@gmail.com>
2019-07-20 18:47:46 +0100

    Implemented UI for patient profile with all the vital values

---
+++ HealthCareManagement/HealthCareManagement/PPPulseOxiMeterVC.swift
@@ -0,0 +1,30 @@
+//
+//  PPPulseOxiMeterVC.swift
+//  HealthCareManagement
+//
+//  Created by Vishwanath Kota on 20/07/19.
+//  Copyright © 2019 University Of Hertfordshire. All rights reserved.
+//
+
+import UIKit
+
+class PPPulseOxiMeterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
